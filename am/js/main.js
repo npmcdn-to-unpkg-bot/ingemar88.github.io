54709994c15adeef9e1e701d3636dc746004926b
Dzambato <kenhim@mail.ru>
2016-07-25 17:01:45 +0300

    kj
    d

--- am/js/main.js
+++ am/js/main.js
@@ -1,3 +1,1 @@
-$(".navbar__toggle").click( function() {
-  $(this).toggleClass("active");
-});
+$(".navbar__toggle").click(function(){$(this).toggleClass("active")}),$(".top-slider").swipe({swipe:function(e,l,t,i,s,a){"left"==l&&$(this).carousel("next"),"right"==l&&$(this).carousel("prev")},allowPageScroll:"vertical"});
